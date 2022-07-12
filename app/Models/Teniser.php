996e107358eaefa0979737b7d162c73cab6e1067
NikolinaDjordjevic <djordjevicnikolina97@gmail.com>
2022-07-13 00:13:38 +0200

    Povezani modeli teniser-trening-teren

---
+++ app/Models/Teniser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Trening;
+
+class Teniser extends Model
+{
+    use HasFactory;
+
+    public function treninzi()
+    {
+        return $this->hasMany(Trening::class);
+    }
+}
---
+++ app/Models/Trening.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Teniser;
+use App\Models\Teren;
+
+class Trening extends Model
+{
+    use HasFactory;
+
+    public function teniser()
+    {
+        return $this->belongsTo(Teniser::class);
+    }
+
+    public function teren()
+    {
+        return $this->belongsTo(Teren::class);
+    }
+}
